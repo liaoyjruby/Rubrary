8f81a81387c030b2bb7edc352f7e90f03979cb61
liaoyjruby <liaoyjruby@ucla.edu>
2023-04-14 09:07:22 -0700

    DESeq / RRHO / PCA adjustments & improvements

--- R/DESeq.R
+++ R/DESeq.R
@@ -11,17 +11,13 @@ utils::globalVariables(c(
 
 #' df_to_mtx
 #'
-#' @param df A dataframe
-#' @param merged If dataframe is made from combined datasets
-#' @param filter Apply filter for genes with 9+ zeros
+#' @param df dataframe
+#' @param filter logical; T to filter out genes with 9+ zeros
 #'
 #' @return Dataframe restructured as matrix
 #' @export
 #'
-df_to_mtx <- function(df, merged = T, filter = T) {
-  if (!merged) {
-    filter <- F
-  }
+df_to_mtx <- function(df, filter = T) {
   mtx <- df
   rownames(mtx) <- df$gene
   mtx <- mtx[, -1]
@@ -45,51 +41,55 @@ df_to_mtx <- function(df, merged = T, filter = T) {
   return(mtx)
 }
 
-run_DESeq <- function(mtx_rawcts, mtx_annotation, outname, merged = F, shrink = F) {
-  mtx <- mtx_rawcts
-  anno <- mtx_annotation
-
-  if (grepl("UCLA&Patch", outname)) {
-    set <- "UCLA&Patch"
-  } else if (grepl("UCLA", outname)) {
-    set <- "UCLA"
-  } else {
-    set <- "Patch"
-  }
-
+#' Run DESeq
+#'
+#' Not sure if generalizable to non OV project related datasets yet...
+#'
+#' @param mtx_rawcts matrix; numeric matrix of raw counts
+#' @param mtx_annotation matrix; samples as rownames, cols "Subject" & "Condition" (& "Batch" if merged)
+#' @param savename string; filename to save outputs under (no ext.)
+#' @param merged logical; T if dataset has two batches merged
+#' @param shrink logical; apply lfcShrink "ashr" to results (NOT IMPLEMENTED)
+#'
+#' @return DESeq results in dataframe
+#' @export
+#'
+run_DESeq <- function(mtx_rawcts, mtx_annotation, savename = NULL,
+                      merged = F, shrink = F) {
   # Paired
   dds_simple <- DESeq2::DESeqDataSetFromMatrix(
-    countData = mtx,
-    colData = anno,
+    countData = mtx_rawcts,
+    colData = mtx_annotation,
     design = ~ Subject + Condition
   )
 
   if (merged) {
-    num_batches <- length(unique(anno$Batch))
+    num_batches <- length(unique(mtx_annotation$Batch))
     subj_n <- c()
     for (i in 1:num_batches) {
-      batch_pairs <- sum(anno$Batch == unique(anno$Batch)[i]) / 2
+      batch_pairs <- sum(mtx_annotation$Batch == unique(mtx_annotation$Batch)[i]) / 2
       subj_n <- c(subj_n, rep(1:batch_pairs, each = 2)) # list of 1-n_batch, together
     }
-    anno$Subject.n <- subj_n
-    anno$Subject.n <- factor(anno$Subject.n)
+    mtx_annotation$Subject.n <- subj_n
+    mtx_annotation$Subject.n <- factor(mtx_annotation$Subject.n)
     # Single DS DESeq should already have Condition / Subject columns
 
-    design <- stats::model.matrix(~ Batch + Batch:Subject.n + Batch:Condition, anno)
+    design <- stats::model.matrix(~ Batch + Batch:Subject.n + Batch:Condition, mtx_annotation)
     design <- design[, colSums(design != 0) > 0]
 
     seq <- DESeq2::DESeq(dds_simple, full = design, betaPrior = FALSE)
-    contrast_list <- list(c(DESeq2::resultsNames(seq)[length(DESeq2::resultsNames(seq)) - 1], DESeq2::resultsNames(seq)[length(DESeq2::resultsNames(seq))]))
+    contrast_list <- list(c(DESeq2::resultsNames(seq)[length(DESeq2::resultsNames(seq)) - 1],
+                            DESeq2::resultsNames(seq)[length(DESeq2::resultsNames(seq))]))
   } else {
     seq <- DESeq2::DESeq(dds_simple)
     contrast_list <- c("Condition", "Recurrent", "Primary")
   }
   DESeq2::resultsNames(seq)
   res <- DESeq2::results(seq, contrast = contrast_list)
-  DESeq2::plotMA(res, ylim = c(-2, 2), main = paste0(set, " DESeq"))
+  DESeq2::plotMA(res, ylim = c(-2, 2), main = "DESeq")
   if (shrink) {
     res_lfc <- DESeq2::lfcShrink(seq, res = res, contrast = contrast_list, type = "ashr")
-    DESeq2::plotMA(res_lfc, ylim = c(-2, 2), main = paste0(set, " DESeq log2 Shrink"))
+    DESeq2::plotMA(res_lfc, ylim = c(-2, 2), main = "DESeq log2 Shrink")
     res_lfc_df <- as.data.frame(res_lfc)
   }
   res_df <- as.data.frame(res)
@@ -97,16 +97,19 @@ run_DESeq <- function(mtx_rawcts, mtx_annotation, outname, merged = F, shrink = 
   resOrdered <- res_df[order(res_df$sign_log_p, decreasing = T), ]
   resOrdered <- resOrdered[stats::complete.cases(resOrdered), ] # excludes rows with NA
   resOrdered <- tibble::rownames_to_column(resOrdered, "gene")
-  utils::write.table(x = resOrdered, file = paste0(outname, ".txt"), sep = "\t", row.names = F, quote = F)
-  rank_file <- resOrdered[c("gene", "sign_log_p")]
-  utils::write.table(rank_file, paste0(outname, ".rnk"), sep = "\t", quote = F, col.names = F, row.names = F)
+  if(!is.null(savename)){
+    utils::write.table(x = resOrdered, file = paste0(savename, ".txt"), sep = "\t", row.names = F, quote = F)
+    rank_file <- resOrdered[c("gene", "sign_log_p")]
+    utils::write.table(rank_file, paste0(savename, ".rnk"), sep = "\t", quote = F, col.names = F, row.names = F)
+  }
+  return(resOrdered)
 }
 
 #' Filter signature to PC coding only by matching with genes in log2UQcounts
 #'
-#' @param sig_path Path to DESeq signature
+#' @param sig_path string; path to DESeq signature
 #'
-#' @return Text and rnk files subsetted to PC coding only genes
+#' @return DESeq signature subsetted to PC only genes
 #' @export
 #'
 make_pconly <- function(sig_path) {
@@ -115,25 +118,21 @@ make_pconly <- function(sig_path) {
   } else {
     rnkonly <- F
   }
-  pathsplit <- strsplit(sig_path, "[_|.]")[[1]]
-  set <- pathsplit[2] # UCLA or Patch or UCLA/Patch
-  slogp <- pathsplit[5] # slogp or slogponly
-  if (set == "Patch") {
-    batch <- ""
-  } else {
-    batch <- paste0("_", pathsplit[length(pathsplit) - 1])
-  }
-  merged_OV_log2UQ <- utils::read.table(paste0(dir.db, "/OV/OV_UCLA&Patch_recur_log2UQcounts.tsv"), header = T) # Filter to genes that are present in the log2UQ table
+
+  pc_genes <- read.delim("/Users/liaoyj/Library/CloudStorage/Dropbox/Ovarian Project/log2_coding_expression_datasets/protein-coding_gene.txt")$symbol
   if (!rnkonly) {
-    sig <- utils::read.delim(paste0(dir.db, sig_path), header = T)
-    sig_pconly <- sig[sig$gene %in% merged_OV_log2UQ$gene, ] # Match with log2UQ coding genes
-    utils::write.table(sig_pconly, paste0(dir.db, "/OV/DESeq/OV_", set, "_recur_prevpost_slogp_DESeq_pconly", batch, ".txt"), sep = "\t", quote = F, col.names = T, row.names = F)
+    sig <- utils::read.delim(sig_path, header = T)
+    sig_pconly <- sig[sig$gene %in% pc_genes, ] # Match with log2UQ coding genes
+    utils::write.table(sig_pconly, paste0(tools::file_path_sans_ext(sig_path), "_pconly.txt"),
+                       sep = "\t", quote = F, col.names = T, row.names = F)
   }
   # Make rank
-  sig_rnk <- utils::read.delim(paste0(dir.db, "/OV/DESeq/OV_", set, "_recur_prevpost_", slogp, "_DESeq", batch, ".rnk"), header = F, col.names = c("Gene", "Score"))
-  sig_pconly_rnk <- sig_rnk[sig_rnk$Gene %in% merged_OV_log2UQ$gene, ] # Match with log2UQ coding genes
-  # sig_pconly_rnk$Score_PC <- 1:nrow(sig_pconly_rnk) # Rerank after filter to protein coding only
-  utils::write.table(sig_pconly_rnk, paste0(dir.db, "/OV/DESeq/OV_", set, "_recur_prevpost_", slogp, "_DESeq_pconly", batch, ".rnk"), sep = "\t", quote = F, col.names = F, row.names = F)
+  sig_rnk <- utils::read.delim(paste0(tools::file_path_sans_ext(sig_path), ".rnk"), header = F, col.names = c("gene", "score"))
+  sig_pconly_rnk <- sig_rnk[sig_rnk$gene %in% pc_genes, ] # Match with log2UQ coding genes
+  utils::write.table(sig_pconly_rnk, paste0(tools::file_path_sans_ext(sig_path), "_pconly.rnk"),
+                     sep = "\t", quote = F, col.names = F, row.names = F)
+
+  return(sig_pconly)
 }
 
 #' plot_DESeq_scatter
@@ -214,169 +213,150 @@ plot_DESeq_scatter <- function(sig1path, sig2path, batch = "",
 
 #' plot_DESeq_boxplot
 #'
-#' @param sig Signature
-#' @param data Gene expression data
-#' @param anno Annotation file for samples
-#' @param batch Descriptive word/phrase appended to output path
-#' @param merged If dataframe is made from combined datasets
+#' @param sig dataframe; sorted signature with genes as first column
+#' @param data dataframe; gene expression data w/ "gene" column
+#' @param anno dataframe; sample annotation file w/ "Batch" & "Type" cols
+#' @param merged logica; T if dataframe is made from combined datasets
+#' @param colors vector; list of colors, n = number of batches
+#' @param savename string; filepath to save figures under (no ext.)
+#' @param width numeric; width of plot saved
+#' @param height numeric; height of plot saved
+#' @param batch string; if not merged,
 #'
-#' @return Paired boxplots comparing pre-post for top/bot 5 DESeq genes, plotting gene expression data
+#' @return Boxplots of gene expression for top/bot 5 genes in sig
 #' @export
 #'
-plot_DESeq_boxplot <- function(sig, data, anno, batch, merged = T) {
-  if (.Platform$OS.type == "unix") {
-    dir.db <- "/Users/liaoyj/Dropbox"
-  } else {
-    dir.db <- "C:/Users/rubsy/Dropbox"
-  }
-  signature <- sig
-  merged_data <- data
-  merged_anno <- anno
-  if (!merged) {
-    merged_anno <- tibble::rownames_to_column(merged_anno, "Sample")
-  }
-  # Get top/bot n=5 genes
-  pos_genes <- signature[1:5, 1]
-  neg_genes <- signature[(nrow(signature) - 4):nrow(signature), 1]
-  top_genes <- pos_genes
-  bottom_genes <- neg_genes
-  pos_genes <- merged_data[merged_data$gene %in% pos_genes, ]
-  neg_genes <- merged_data[merged_data$gene %in% neg_genes, ]
-  pos_genes <- pos_genes[match(top_genes, pos_genes$gene), ]
-  neg_genes <- neg_genes[match(bottom_genes, neg_genes$gene), ]
-  # Set row names to genes and rm gene column
-  row.names(pos_genes) <- pos_genes$gene
-  pos_genes <- pos_genes[-1]
-  row.names(neg_genes) <- neg_genes$gene
-  neg_genes <- neg_genes[-1]
-  # Transpose
-  pos_genes <- as.data.frame(t(pos_genes))
-  pos_genes <- data.frame(Sample = rownames(pos_genes), pos_genes)
-  rownames(pos_genes) <- c() # Rm rownames
-  neg_genes <- as.data.frame(t(neg_genes))
-  neg_genes <- data.frame(Sample = rownames(neg_genes), neg_genes)
-  rownames(neg_genes) <- c()
+plot_DESeq_boxplot <- function(sig, data, anno, merged = T, batch = "UCLA",
+                               colors = c("goldenrod2", "navyblue"),
+                               savename = NULL, width = 13, height = 9) {
 
-  # Merge with annotation file to get Batch (UCLA/Patch) and Condition (Primary/Recurrent) info
-  pos_input <- merge(pos_genes, merged_anno)
-  neg_input <- merge(neg_genes, merged_anno)
-  pos_input <- pos_input[c(1:8)]
-  neg_input <- neg_input[c(1:8)]
+  # Get top 5 genes
+  pos_genes <- sig[1:5, 1]
+  top_genes <- pos_genes
+  pos_genes <- data[data$gene %in% pos_genes, ]
+  pos_genes <- pos_genes[match(top_genes, pos_genes$gene), ]
+  rownames(pos_genes) <- NULL
+  pos_genes <- tibble::column_to_rownames(pos_genes, var = "gene")
+  pos_genes <- as.data.frame(t(pos_genes))
+  pos_genes <- tibble::rownames_to_column(pos_genes, var = "Sample")
+
+  # Get bot 5 genes
+  neg_genes <- sig[(nrow(sig) - 4):nrow(sig), 1]
+  bottom_genes <- neg_genes
+  neg_genes <- data[data$gene %in% neg_genes, ]
+  neg_genes <- neg_genes[match(bottom_genes, neg_genes$gene), ]
+  rownames(neg_genes) <- NULL
+  neg_genes <- tibble::column_to_rownames(neg_genes, var = "gene")
+  neg_genes <- as.data.frame(t(neg_genes))
+  neg_genes <- tibble::rownames_to_column(neg_genes, var = "Sample")
+
+  # Merge with annotation file
+  pos_input <- dplyr::left_join(pos_genes, tibble::rownames_to_column(anno, var = "Sample"), by = "Sample")
+  neg_input <- dplyr::left_join(neg_genes, tibble::rownames_to_column(anno, var = "Sample"), by = "Sample")
+  pos_input <- pos_input[,c("Sample", top_genes, "Batch", "Type")]
+  neg_input <- neg_input[,c("Sample", bottom_genes, "Batch", "Type")]
   colnames <- c("Sample", "gene1", "gene2", "gene3", "gene4", "gene5", "Batch", "Type")
-  pos_gene_names <- colnames(pos_input)[2:6]
-  neg_gene_names <- colnames(neg_input)[2:6]
   colnames(pos_input) <- colnames
   colnames(neg_input) <- colnames
 
-  colors <- c("goldenrod2", "navyblue")
-
   if (!merged) {
-    pos_input$Type <- pos_input$Batch
-    pos_input$Batch <- "UCLA"
-    neg_input$Type <- neg_input$Batch
-    neg_input$Batch <- "UCLA"
-    colors <- c("navyblue")
+    pos_input$Batch <- batch
+    neg_input$Batch <- batch
   }
 
   # Plot positive
   pos_gene_1 <- ggplot2::ggplot(pos_input, ggplot2::aes(Type, gene1)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Pos ", pos_gene_names[1]),
+      title = paste0("Pos ", top_genes[1]),
       y = "Gene Exp"
     )
   pos_gene_2 <- ggplot2::ggplot(pos_input, ggplot2::aes(Type, gene2)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Pos ", pos_gene_names[2]),
+      title = paste0("Pos ", top_genes[2]),
       y = "Gene Exp"
     )
   pos_gene_3 <- ggplot2::ggplot(pos_input, ggplot2::aes(Type, gene3)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Pos ", pos_gene_names[3]),
+      title = paste0("Pos ", top_genes[3]),
       y = "Gene Exp"
     )
   pos_gene_4 <- ggplot2::ggplot(pos_input, ggplot2::aes(Type, gene4)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Pos ", pos_gene_names[4]),
+      title = paste0("Pos ", top_genes[4]),
       y = "Gene Exp"
     )
   pos_gene_5 <- ggplot2::ggplot(pos_input, ggplot2::aes(Type, gene5)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Pos ", pos_gene_names[5]),
+      title = paste0("Pos ", top_genes[5]),
       y = "Gene Exp"
     )
 
   # Plot negative
   neg_gene_1 <- ggplot2::ggplot(neg_input, ggplot2::aes(Type, gene1)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Neg ", neg_gene_names[1]),
+      title = paste0("Neg ", bottom_genes[1]),
       y = "Gene Exp"
     )
   neg_gene_2 <- ggplot2::ggplot(neg_input, ggplot2::aes(Type, gene2)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Neg ", neg_gene_names[2]),
+      title = paste0("Neg ", bottom_genes[2]),
       y = "Gene Exp"
     )
   neg_gene_3 <- ggplot2::ggplot(neg_input, ggplot2::aes(Type, gene3)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Neg ", neg_gene_names[3]),
+      title = paste0("Neg ", bottom_genes[3]),
       y = "Gene Exp"
     )
   neg_gene_4 <- ggplot2::ggplot(neg_input, ggplot2::aes(Type, gene4)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Neg ", neg_gene_names[4]),
+      title = paste0("Neg ", bottom_genes[4]),
       y = "Gene Exp"
     )
   neg_gene_5 <- ggplot2::ggplot(neg_input, ggplot2::aes(Type, gene5)) +
-    ggplot2::geom_boxplot() +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
     ggplot2::geom_jitter(ggplot2::aes(col = Batch), width = 0.05, height = 0) +
     ggplot2::scale_color_manual(values = colors) +
     ggplot2::theme_classic() +
     ggplot2::labs(
-      title = paste0("Neg ", neg_gene_names[5]),
+      title = paste0("Neg ", bottom_genes[5]),
       y = "Gene Exp"
     )
-
-  if (merged) {
-    title <- "OV_UCLA&Patch_recur_DESeq_log2UQ"
-  } else {
-    title <- "OV_UCLA_recur_DESeq_log2UQ"
-  }
 
   # Combine to png
   posGrid <- gridExtra::arrangeGrob(
@@ -387,8 +367,8 @@ plot_DESeq_boxplot <- function(sig, data, anno, batch, merged = T) {
     pos_gene_5
   )
   ggplot2::ggsave(
-    filename = paste0(dir.db, "/OV/DESeq/", title, "_Pos_", batch, ".png"),
-    plot = posGrid, units = "in", width = 13, height = 9, dpi = 300
+    filename = paste0(savename, "_Pos.png"),
+    plot = posGrid, units = "in", width = width, height = height, dpi = 300
   )
 
   negGrid <- gridExtra::arrangeGrob(
@@ -399,7 +379,7 @@ plot_DESeq_boxplot <- function(sig, data, anno, batch, merged = T) {
     neg_gene_5
   )
   ggplot2::ggsave(
-    filename = paste0(dir.db, "/OV/DESeq/", title, "_Neg_", batch, ".png"),
-    plot = negGrid, units = "in", width = 13, height = 9, dpi = 300
+    filename = paste0(savename, "_Neg.png"),
+    plot = negGrid, units = "in", width = width, height = height, dpi = 300
   )
 }
--- R/plot_pca.R
+++ R/plot_pca.R
@@ -4,6 +4,9 @@ utils::globalVariables(c(
 
 #' Plot PCA with annotation
 #'
+#' @import ggplot2
+#' @importFrom utils read.delim
+#'
 #' @param df string; Path to PC output
 #' @param dfanno string or df; Annotation info for DF
 #' @param PCx string; Component on x-axis
@@ -14,21 +17,22 @@ utils::globalVariables(c(
 #' @param annotype string; Colname in dfanno with info to color by
 #' @param annotype2 string; Colname in dfanno with info to change shape by
 #' @param title string; Plot title
-#' @param marginal logical; Show density plot along both axes
-#' @param savename string; Output plot name
-#' @param highlight char vector; Specific points to shape differently/labels
 #' @param subtitle string; Subtitle for plot
+#' @param density logical; Show density plot along both axes
+#' @param highlight char vector; Specific points to shape differently/labels
+#' @param savename string; filepath to save plot under
+#' @param width numeric; plot width
+#' @param height numeric; plot width
 #'
 #' @return PCA output plotted with annotation
 #'
-#' @importFrom ggplot2 ggplot aes aes_string geom_point labs theme_bw ggsave annotation_custom guide_legend guides guide_colorbar scale_color_gradient
-#' @importFrom utils read.delim
 #' @export
 #'
-plot_PCA <- function(df, dfanno = NA, PCx = "PC1", PCy = "PC2", PCtype = "Score",
-                     label = TRUE, annoname = "Sample", annotype = "Batch", annotype2 = NA, highlight = NA,
+plot_PCA <- function(df, dfanno = NULL, PCx = "PC1", PCy = "PC2", PCtype = "Score",
+                     label = TRUE, annoname = "Sample", annotype = "Batch", annotype2 = NULL, highlight = NULL,
                      title = paste0("PCA ", PCtype, " Plot - ", annotype),
-                     subtitle = NA, marginal = FALSE, savename = NA) {
+                     subtitle = NULL, density = FALSE, savename = NULL,
+                     width = 8, height = 8) {
   if (is.character(df)) { # Read df from path
     dfpath <- df
     df <- read.delim(dfpath)
@@ -45,13 +49,21 @@ plot_PCA <- function(df, dfanno = NA, PCx = "PC1", PCy = "PC2", PCtype = "Score"
     }
   }
 
-  if (all(is.na(dfanno))) { # No coloring / annotation
-    plt <- ggplot(df, aes_string(x = PCx, y = PCy)) +
+  if(exists("sdev")){
+    PCxlab <- paste0(PCx, " (", sdev$pve[match(PCx, rownames(sdev))], "%)")
+    PCylab <- paste0(PCy, " (", sdev$pve[match(PCy, rownames(sdev))], "%)")
+  } else {
+    PCxlab <- PCx
+    PCylab <- PCy
+  }
+
+  if (all(is.null(dfanno))) { # No coloring / annotation
+    plt <- ggplot(df, aes(x = .data[[PCx]], y = .data[[PCy]])) +
       geom_point(size = 2) +
       labs(
         title = title,
-        x = paste0(PCx, " (", sdev$pve[match(PCx, rownames(sdev))], "%)"),
-        y = paste0(PCy, " (", sdev$pve[match(PCy, rownames(sdev))], "%)")
+        x = PCxlab,
+        y = PCylab
       ) +
       theme_classic() +
       {
@@ -63,7 +75,7 @@ plot_PCA <- function(df, dfanno = NA, PCx = "PC1", PCy = "PC2", PCtype = "Score"
     }
     df[, 2:ncol(df)] <- sapply(df[, 2:ncol(df)], as.numeric)
 
-    if (is.na(annotype2)) {
+    if (is.null(annotype2)) {
       df_merged <- dplyr::left_join(df, dfanno[, c(annoname, annotype)], by = structure(names = PCtype, .Data = annoname))
       df_merged <- df_merged[, c(PCtype, PCx, PCy, annotype)]
     } else {
@@ -132,48 +144,48 @@ plot_PCA <- function(df, dfanno = NA, PCx = "PC1", PCy = "PC2", PCtype = "Score"
       guidetitle <- guide_legend(title = annotype)
     }
 
-    if (!all(is.na(highlight))) {
+    if (!all(is.null(highlight))) {
       label <- F
       df_merged$Highlight <- ifelse(df_merged$Score %in% highlight, "HL", "")
-      ggplt <- ggplot(df_merged, aes_string(x = PCx, y = PCy, color = annotype, shape = "Highlight"))
-    } else if (!is.na(annotype2)) {
-      ggplt <- ggplot(df_merged, aes_string(x = PCx, y = PCy, color = annotype, shape = annotype2))
+      ggplt <- ggplot(df_merged, aes(x = .data[[PCx]], y = .data[[PCy]], color = .data[[annotype]], shape = "Highlight"))
+    } else if (!is.null(annotype2)) {
+      ggplt <- ggplot(df_merged, aes(x = .data[[PCx]], y = .data[[PCy]], color = .data[[annotype]], shape = .data[[annotype2]]))
     } else {
-      ggplt <- ggplot(df_merged, aes_string(x = PCx, y = PCy, color = annotype))
+      ggplt <- ggplot(df_merged, aes(x = .data[[PCx]], y = .data[[PCy]], color = .data[[annotype]]))
     }
 
     plt <- ggplt +
-      geom_point(size = 3, alpha = alpha) +
+      geom_point(size = 2, alpha = alpha) +
       {
         if (is.numeric(df_merged[, annotype])) scale_color_gradient(low = "blue", high = "red", guide = "colourbar")
       } +
       labs(
         title = title,
-        x = paste0(PCx, " (", sdev$pve[match(PCx, rownames(sdev))], "%)"),
-        y = paste0(PCy, " (", sdev$pve[match(PCy, rownames(sdev))], "%)")
+        x = PCxlab,
+        y = PCylab
       ) +
       {
-        if (!is.na(subtitle)) labs(subtitle = subtitle)
+        if (!is.null(subtitle)) labs(subtitle = subtitle)
       } +
       guides(color = guidetitle) + # Set title of legend
       theme_classic() +
       {
         if (label) ggrepel::geom_text_repel(label = df[, PCtype], max.overlaps = 30, color = "black")
       } + {
-        if (!all(is.na(highlight))) ggrepel::geom_text_repel(aes(label = ifelse(Highlight == "HL", df[, PCtype], "")), color = "black", max.overlaps = 30, size = 3)
+        if (!all(is.null(highlight))) ggrepel::geom_text_repel(aes(label = ifelse(Highlight == "HL", df[, PCtype], "")), color = "black", max.overlaps = 30, size = 3)
       }
   }
 
-  if (!is.na(savename)) {
+  if (!is.null(savename)) {
     ggsave(
       plot = plt,
       filename = savename,
-      height = 8,
-      width = 8
+      height = height,
+      width = width
     )
   }
 
-  if (marginal && !is.na(dfanno)) {
+  if (density && !is.null(dfanno)) {
     if (length(unique(df_merged[, annotype])) == 2) {
       plt <- plt + annotation_custom(grobX) + annotation_custom(grobY)
     }
@@ -181,17 +193,17 @@ plot_PCA <- function(df, dfanno = NA, PCx = "PC1", PCy = "PC2", PCtype = "Score"
       y = "Density", type = "density", margins = "both",
       size = 6, groupColour = T, groupFill = T
     )
-    if (!is.na(savename)) {
-      savename <- paste0(tools::file_path_sans_ext(savename), "_marginal.png")
+    if (!is.null(savename)) {
+      savename <- paste0(tools::file_path_sans_ext(savename), "_density.png")
 
       ggsave(
         plot = mplt,
         filename = savename,
-        height = 8,
-        width = 8
+        height = height,
+        width = width
       )
     }
-    plt <- mplt
+    # plt <- mplt
   }
 
   plt
