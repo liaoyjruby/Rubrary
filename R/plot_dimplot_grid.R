2842cb02ac9e5f88862733555d5946b67f0dd0f9
liaoyjruby <liaoyjruby@ucla.edu>
2023-04-21 13:18:45 -0700

    plot_dimplot_grid: rename, add title/subtitle options

--- R/plot_dimplot_grid.R
+++ R/plot_dimplot_grid.R
@@ -2,21 +2,25 @@
 #' Plot Seurat DimPlot grid set
 #'
 #' @param sobj Seurat object
-#' @param reduction c("pca", "tsne", "umap); must be already run in sobj
+#' @param reduction c("pca", "tsne", "umap); must be already present in sobj
 #' @param set dataframe or vector; col1 metadata name, col2 (opt) subplot description, col3 (opt) logical T/F for label
 #' @param ncol number of columns in grid
 #' @param guides string; specify how guides should be treated in layout (?patchwork::wrap_plots)
 #' @param redtitle logical; `TRUE` to prepend reduction method in subplot title
+#' @param gridtitle string; title for overall dimplot grid
+#' @param gridsubtitle string; subtitle for overall dimplot grid
 #' @param savename string; filepath to save figure under
 #' @param width integer; width of plot
 #' @param height integer; height of plot
 #'
 #' @return Patchwork grid of Seurat DimPlots
 #' @export
-plot_dim_set <- function(sobj, reduction = "umap", set, ncol = 3,
-                         guides = NULL, redtitle = FALSE,
+plot_dimplot_grid <- function(sobj, reduction = c("umap", "pca", "tsne"), set, ncol = 3,
+                         guides = NULL, redtitle = FALSE, gridtitle = NULL, gridsubtitle = NULL,
                          savename = NULL, width = NULL, height = NULL){
   Rubrary::use_pkg("Seurat")
+
+  reduction <- match.arg(reduction)
 
   plot_dim <- function(group, name, lb){
     red <- ifelse(reduction == "tsne", "tSNE", toupper(reduction))
@@ -49,7 +53,10 @@ plot_dim_set <- function(sobj, reduction = "umap", set, ncol = 3,
   if(ncol(set) == 2){ set[,3] = FALSE}
 
   plts <- mapply(plot_dim, set[,1], set[,2], set[,3], SIMPLIFY = FALSE)
-  dim_set <- patchwork::wrap_plots(plts, ncol = ncol, guides = guides)
+  dim_set <- patchwork::wrap_plots(plts, ncol = ncol, guides = guides) +
+    patchwork::plot_annotation(
+      title = gridtitle,
+      subtitle = gridsubtitle)
 
   if(!is.null(savename)) {
     ggplot2::ggsave(
